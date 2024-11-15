cd370f118e2f12106f53e0ebea98fcddbfc47130
junemel610 <meljune610@gmail.com>
2024-11-16 02:05:24 +0800

    Logic added for adding a wood analytic and accessing it.

--- Backend/controllers/woodController.js
+++ Backend/controllers/woodController.js
@@ -1,7 +1,38 @@
+const jwt = require('jsonwebtoken');
+const wood = require('../models/analytics');
+
 exports.createWoodprof = async (req, res) => {
-// Insert Logic
+    try {
+        const { woodCount, defectType, defectNo, woodClassification, date, time } = req.body;
+
+        const wood = new Analytic({
+            woodCount,
+            defectType,
+            defectNo,
+            woodClassification,
+            date: new Date(date), 
+            time, 
+        });
+
+        await wood.save();
+
+        res.json({ success: true, wood });
+    } catch (error) {
+        console.error('Error creating wood profile:', error);
+        res.status(500).json({ message: 'Internal server error', success: false });
+    }
 };
 
 exports.getWoodData = async (req, res) => {
-// Insert Logic
+    try {
+        const woodData = await Analytic.find(); // Use the model to fetch data
+
+        res.json({
+            success: true,
+            woodData,
+        });
+    } catch (error) {
+        console.error('Error fetching wood data:', error);
+        res.status(500).json({ success: false, message: 'Internal server error' });
+    }
 };
--- Backend/models/analytics.js
+++ Backend/models/analytics.js
@@ -18,7 +18,7 @@ const AnalyticsSchema = new mongoose.Schema({
         required: true
     },
     date: {
-        type: String,
+        type: Date,
         required: true
     },
     time: {
