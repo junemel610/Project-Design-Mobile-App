5ced801a0ba86680f07d9297c8dddcdef688ff7c
junemel610 <meljune610@gmail.com>
2024-12-04 15:52:42 +0800

    Removed bcrypt temporarily

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const nodemailer = require('nodemailer');
 require('dotenv').config();
 require('./models/db');
 const userRouter = require('./routes/inspectura');
